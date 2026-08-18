27db54931703299fafc87f87f9850f1cb9e0dd66
muhammed bahadi <muhammedbahadi925@gmail.com>
2026-08-18 12:50:37 +0300

    feat/ add domain AuditableEntity

---
+++ src/MechanicShop.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,17 @@
+using MechanicShop.Domain.Common.Constants;
+
+namespace MechanicShop.Domain.Common;
+
+class AuditableEntity : Entity
+{
+    protected AuditableEntity()
+    {}
+
+    protected AuditableEntity(Guid id) : base(id)
+    {}
+
+    public DateTimeOffset CreatedAtUtc { get; set; }
+    public string? CreatedBy { get; set; }
+    public DateTimeOffset LastModifiedUtc { get; set; }
+    public string? LastModifiedBy { get; set; }
+}
