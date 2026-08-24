dce56bb2d8ffefeb1afe5006091ad9e8a3b6e805
muhammed bahadi <muhammedbahadi925@gmail.com>
2026-08-24 20:05:15 +0300

    feat/added the domain events to track any changes

--- src/MechanicShop.Domain/Common/Entity.cs
+++ src/MechanicShop.Domain/Common/Entity.cs
@@ -4,11 +4,28 @@ public abstract class Entity
 {
     public Guid Id { get; set; } 
 
+    private readonly List<DomainEvent> _domainEvents = [];
+
     protected Entity()
     {}
 
     protected Entity(Guid id)
     {
         Id = id == Guid.Empty ? Guid.NewGuid() : id;
+    }
+
+    public void AddDomainEvent(DomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+
+    public void RemoveDomainEvent(DomainEvent domainEvent)
+    {
+        _domainEvents.Remove(domainEvent);
+    }
+
+    public void CleardomainEvent(DomainEvent domainEvent)
+    {
+        _domainEvents.Clear();
     }
 }
